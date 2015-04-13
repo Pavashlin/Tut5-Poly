ffa737b742b44f0809a522910d3f98ee6aae60e8
Pavashlin <pavashlingovender@yahoo.com>
2015-04-13 19:34:32 +0200

    add comments

--- ConsoleApplication1/Shape.h
+++ ConsoleApplication1/Shape.h
@@ -3,10 +3,10 @@
 #define SHAPE_H
 using namespace std;
 
-class Shape
+class Shape										//abstract class
 {
 public:
-	virtual void name() = 0;
+	virtual void name() = 0;					//pure virtual functions
 	virtual void perimeter() = 0;
 	virtual void area() = 0;
 	virtual void draw() = 0;
--- ConsoleApplication1/Tut5-Poly.cpp
+++ ConsoleApplication1/Tut5-Poly.cpp
@@ -16,9 +16,17 @@ int _tmain(int argc, _TCHAR* argv[])
 	Circle bob(5);
 	Shape *shape1 = &bob;
 	shape1->name();
+	shape1->perimeter();
+	shape1->area();
+	shape1->draw();
 
-	cout << "*****Rectangle*****" << endl;
-	Rectangle Jim()
+	cout << "*****Rectangle*****" << endl;								//testing rectangle
+	Rectangle Jim(4, 5);
+	Shape *shape2 = &Jim;
+	shape2->name();
+	shape2->perimeter();
+	shape2->area();
+	shape2->draw();
 	return 0;
 }
 
